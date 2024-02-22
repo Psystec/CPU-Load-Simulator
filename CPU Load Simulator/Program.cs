1d62d28587302a5fa4369bffb15bfb93b0bc961f
Psystec <12035424+Psystec@users.noreply.github.com>
2024-02-22 11:05:24 +0200

    Add files via upload

---
+++ CPU Load Simulator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace CPU_Load_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cpuUsage = 100;
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(CPUKill));
+                t.Start(cpuUsage);
+                threads.Add(t);
+            }
+            Thread.Sleep(int.MaxValue);
+        }
+
+        public static void CPUKill(object cpuUsage)
+        {
+            Parallel.For(0, 1, new Action<int>((int i) =>
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                while (true)
+                {
+                    if (watch.ElapsedMilliseconds > (int)cpuUsage)
+                    {
+                        Thread.Sleep(100 - (int)cpuUsage);
+                        watch.Reset();
+                        watch.Start();
+                    }
+                }
+            }));
+
+        }
+    }
+}
